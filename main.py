d23db8864912bed85c469d63076943f98fc33723
MSuiram <marius.valard@gmail.com>
2026-01-26 13:58:37 +0100

    Add picture on marker

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import folium
 import os
 from marker_maker import marker_maker
 from coord import coord_calculator
+from secret import path
 
 my_map = folium.Map()
 
-path_name = 'path'
+path_name = path
 images = [img for img in os.listdir(path_name) if img.endswith(".jpg")]
 print(images)
 
 for image in images:
-    coord = coord_calculator(path_name + "/" + image)
+    path_image = path_name + "/" + image
+    coord = coord_calculator(path_image)
     if coord is not None:
         print(image)
-        marker_maker(my_map, coord, image)
-
+        marker_maker(my_map, coord, path_image, image)
 
 my_map.save("my_map.html")
--- marker_maker.py
+++ marker_maker.py
@@ -1,8 +1,11 @@
 import folium
 
-def marker_maker(map : folium.Map, loc : list, popup : str):
+def marker_maker(map : folium.Map, loc : list, image : str, name : str):
     folium.Marker(
         location= loc,
         tooltip= "Click !!",
-        popup= popup,
+        popup= f"""<div>
+            <img src={image} width="230" height="172">
+            <br /><span>{name}</span>
+            </div>""",
     ).add_to(map)
